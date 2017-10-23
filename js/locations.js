ee86117a0f934468c12fc1a35087b436a3179ab5
Abdul Rasheed <narejo@gmail.com>
2017-10-23 12:12:47 +1100

    initialize

---
+++ js/locations.js
@@ -0,0 +1,21 @@
+// map center location
+let center = {lat: -35.2914808, lng: 149.1296499};
+
+// areas of interest in canberra
+let myLocations = [
+  {
+    name: 'National Museum of Australia',
+    location: {lat: -35.2933, lng: 149.1211},
+    fsId: '4b76af8ff964a5206c582ee3'
+  },
+  {
+    name: 'National Zoo & Acquarium',
+    location: {lat: -35.2988, lng: 149.0703},
+    fsId: '4ba42cdbf964a520208938e3'
+  },
+  {
+    name: 'Big Splash Waterpark',
+    location: {lat: -35.2513, lng: 149.0692},
+    fsId: '4b48b2cff964a520365326e3'
+  }
+];
---
+++ js/mapstyle.js
@@ -0,0 +1,89 @@
+// Custom map style courtesy snazzymaps.com
+
+let customStyle = [
+  {
+    "featureType": "poi",
+    "elementType": "labels",
+    "stylers": [
+      {
+        "visibility": "on"
+      }
+    ]
+  },
+  // end - remove Places of Interest labels
+  {
+    "featureType": "landscape",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "visibility": "on"
+      },
+      {
+        "color": "#e8e7e3"
+      }
+    ]
+  },
+  {
+    "featureType": "landscape.natural",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#f8f8f6"
+      }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "visibility": "on"
+      },
+      {
+        "color": "#118983"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "labels",
+    "stylers": [
+      {
+        "lightness": 19
+      },
+      {
+        "saturation": -100
+      }
+    ]
+  },
+  {
+    "featureType": "transit.station",
+    "elementType": "all",
+    "stylers": [
+      {
+        "visibility": "on"
+      }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "visibility": "on"
+      },
+      {
+        "color": "#cde0fd"
+      }
+    ]
+  }
+];
